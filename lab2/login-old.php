010a39dde9dc5d61c52566f3cfed2596d4a9dd04
vsoljic <vedrana.soljic1@gmail.com>
2016-04-14 10:19:27 +0200

    Added files via upload

---
+++ lab2/login-old.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Fantastic Beasts</title>
+  
+
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/grid.css" />
+  <link rel="stylesheet" href="css/stil.css" />
+</head>
+
+<body>
+  <header class="site-header-logged">
+    <div class="row">
+      <div class="column column-4">
+        <img src="img/fantBeastsLogo2.png">
+      </div>
+
+      <div class="column column-8">
+        <div class="odjava">
+        	<form method="post" action="login.html">
+        	<?php echo $_GET["username"]; ?>
+     
+      	<input type="submit" name="login" value="Odjava">
+  		</form>
+      </div>
+      </div> 
+      
+
+
+
+    </div>
+  </header>
+  
+ 
+  <section class="body">
+    <div class="row">
+      <div class="column column-4">
+      <nav class="main-navigation">
+        <ul>
+          <h2>IZBORNIK</h2>
+          <li><a href="#">Opcija 2</a></li>
+          <li><a href="#">Opcija 3</a></li>
+          <li><a href="#">Opcija 4</a></li>
+          <li><a href="#">Opcija 5</a></li>
+        </ul>
+      </nav>
+    </div>
+
+      <div class="sadrzaj column column-8"> 
+       
+        <h2>Osobni podaci</h2>
+        <p>Vedrana Šoljić</p>
+          <p> Braće Domany 6, Srednjaci, 10000 Zagreb (Hrvatska) </p>   
+
+          <p> vedrana.soljic1@gmail.com  </p>
+
+          <p> Datum rođenja 21/08/1994 </p>
+          <p> Državljanstvo hrvatsko </p>
+          <p> Mjesto rođenja: Zagreb, Hrvatska</p>
+        </p>
+        <br>
+        <h2>Podaci o školovanju </h2>
+        <p>
+          <b>23 srpnja 13–danas </b> Stručnik prvostupnik inženjer računarstva razina 6 EKO-a
+          Tehničko Veleučilište u Zagrebu, Zagreb (Hrvatska) </p>
+      
+      <p>
+      <b>6 rujna 09–15 lipnja 13 </b>  razina 4 EKO-a,
+        XI. gimnazija
+      Savska cesta 77, 10000 Zagreb (Hrvatska) 
+
+        </p>
+
+      <p>
+        <b>rujan 2001-lipanj 09 </b> OŠ Trnsko
+      </p>
+
+        <br><br>
+
+        <h2>Podaci o radnom iskustvu </h2>
+        <p>
+          <b>1 srpnja 13–15 rujna 13</b> Rad u skladištu
+          VM2
+        Rudeška 14, 10000 Zagreb (Hrvatska) 
+          - deklariranje proizvoda
+         </p>
+
+         <p>
+        <b>1 lipnja 14–danas </b>Statist
+          Hrvatska Radiotelevizija
+        Prisavlje 3, 10000 Zagreb (Hrvatska) 
+          - statiranje na raznim emisijama
+        </p>
+        <br><br>
+
+        <h2>Znanje i vještine</h2>
+        <p>
+
+        jezici: engleski jezik, njemački jezik</p>
+
+        <p>Sklona timskom radu, sposobnost prilagodbe, komunikativna
+        Stečene sposobnosti zahvaljujući glazbenoj školi, solfeggio-u te orkestru, također plesanju rock'n'roll-a u grupi</p>
+
+        <p>
+          Vješto korištenje alata u MS Office-u – znanje stečeno u osnovnoj i srednjoj školi (Informatika), te na fakultetu na kolegiju “Primjena računala”
+          Programski jezici: C, C++, C#, Java, PHP, SQL, Javascript
+        </p>
+      </div>
+
+      
+  </section> 
+<br><br><br>
+
+   <footer class="site-footer">
+    <div class="row">
+    <div class="column column-12"> <br>
+      <p><b>&copy Copyright Fantastic Beats, 2016</b></p>
+    </div>
+
+    </div>
+  </footer>
+</body>
+</html>
